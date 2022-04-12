713a84ca0d62e5efdcdb37d94232017d8b15a275
Kostas Siras <kostas.siras@5J2T013.internal.upstreamsystems.com>
2022-04-12 17:39:16 +0300

    Implemented some unit testing on part_1 of the exercise

--- src/test/java/com/upstreamsystems/trainee/challenge1/service/OrderServiceTest.java
+++ src/test/java/com/upstreamsystems/trainee/challenge1/service/OrderServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import com.upstreamsystems.trainee.challenge1.model.Order;
 import com.upstreamsystems.trainee.challenge1.model.OrderSuggestion;
 import com.upstreamsystems.trainee.challenge1.utils.CSVReader;
+
 import java.util.List;
 
 import com.upstreamsystems.trainee.challenge1.utils.CSVReaderTester;
@@ -13,13 +14,13 @@ import org.junit.Test;
 
 public class OrderServiceTest {
 
-  OrderService service;
+    OrderService service;
 
-  @Before
-  public void init() {
-    service = new OrderServiceImpl(CSVReader.getShops(),
-        CSVReader.getProductPrices(), CSVReader.getCities());
-  }
+    @Before
+    public void init() {
+        service = new OrderServiceImpl(CSVReader.getShops(),
+                CSVReader.getProductPrices(), CSVReader.getCities());
+    }
 
 //  @Test
 //  public void test_combinations() throws Exception {
@@ -28,29 +29,49 @@ public class OrderServiceTest {
 //    assertEquals(15, suggestions.size());
 //  }
 
-  @Test
-  public void test_non_existing_product() throws Exception {
-    List<OrderSuggestion> suggestions = service.calculate(
-        new Order("user1", List.of("product_non_exist1", "product_non_exist2")));
-    assertEquals(0, suggestions.size());
-  }
-
-  @Test
-  public void test_discount_is_applied() throws Exception {
-    CSVReaderTester csvReaderTester = new CSVReaderTester("/productstest1.csv","/shopstest1.csv","/citiestest1.csv");
-    OrderService service = new OrderServiceImpl(csvReaderTester.getShops(),
-            csvReaderTester.getProductPrices(), csvReaderTester.getCities());
-
-    List<String> orderProducts = List.of("product1");
-
-    List<OrderSuggestion> orderSuggestions = service.calculate(new Order("user1",orderProducts));
-
-    int[] cost = {120,500,580};
-    int cnt = 0;
-    for (OrderSuggestion suggestion : orderSuggestions){
-      assertEquals(cost[cnt],suggestion.getTotalCost());
-      System.out.println(cnt++ + "=> " +suggestion);
+    @Test
+    public void test_non_existing_product() throws Exception {
+        List<OrderSuggestion> suggestions = service.calculate(
+                new Order("user1", List.of("product_non_exist1", "product_non_exist2")));
+        assertEquals(0, suggestions.size());
     }
-  }
+
+    @Test
+    public void test_discount_is_applied() throws Exception {
+        CSVReaderTester csvReaderTester = new CSVReaderTester("/productstest1.csv", "/shopstest1.csv", "/citiestest1.csv");
+        OrderService service = new OrderServiceImpl(csvReaderTester.getShops(),
+                csvReaderTester.getProductPrices(), csvReaderTester.getCities());
+
+        List<String> orderProducts = List.of("product1");
+
+        List<OrderSuggestion> orderSuggestions = service.calculate(new Order("user1", orderProducts));
+
+        int[] cost = {120, 500, 580};
+        int cnt = 0;
+        for (OrderSuggestion suggestion : orderSuggestions) {
+            assertEquals(cost[cnt], suggestion.getTotalCost());
+            System.out.println(cnt++ + "=> " + suggestion);
+        }
+    }
+
+    @Test
+    public void test_premium_order_calculated() throws Exception {
+        CSVReaderTester csvReaderTester = new CSVReaderTester("/productstest2.csv", "/shopstest2.csv", "/citiestest1.csv");
+        OrderService service = new OrderServiceImpl(csvReaderTester.getShops(),
+                csvReaderTester.getProductPrices(), csvReaderTester.getCities());
+
+        List<String> orderProducts = List.of("product1", "product2");
+
+
+        List<OrderSuggestion> orderSuggestions = service.calculate(new Order("user1", orderProducts, true));
+
+
+        int cnt = 0;
+        int[] cost = {70, 90, 90, 110};
+        for (OrderSuggestion suggestion : orderSuggestions) {
+            assertEquals(cost[cnt], suggestion.getTotalCost());
+            System.out.println(cnt++ + "=> " + suggestion);
+        }
+    }
 
 }
